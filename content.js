844323abce6223ac515b7bc54cc5df6a5a5ecce4
tsutoringo <tsutoringo+akaharaimorinotsutoringo@gmail.com>
2020-06-04 09:32:29 +0900

    Update

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 function getTime(cle) {let Time = 0;for(var i=0;i<cle.length;i++)Time+=Number(cle[i].innerText.substr(0,2))*60+Number(cle[i].innerText.substr(-2));return Time;}
+function getCount(className) { return {all :document.getElementsByClassName(className).length,good: document.querySelectorAll(`.good.${className}`).length}}
 const format = Time => (`${Math.floor(Time/3600)}時間${Math.floor((Time%3600)/60)}分${(Time%60)}秒`)
+
 let goodtime = getTime(document.querySelectorAll(".good .movie-length"));
 let time      = getTime(document.getElementsByClassName('movie-length'));
 
@@ -7,14 +9,14 @@ let hyouji = document.getElementsByClassName('description');
 
 if(hyouji === undefined) location.reload();
 
-let movieCount = document.getElementsByClassName('movie').length;
-let evaluationTestCount = {
-	all :document.getElementsByClassName('evaluation-test').length,
-	good: document.querySelectorAll('.good.evaluation-test').length
+let movieCount = getCount("movie");
+let testCount  = {
+	essay : getCount("essay-test"),
+	evaluation: getCount("evaluation-test")
 }
-let essayTestCount = {
-	all :document.getElementsByClassName('essay-test').length,
-	good: document.querySelectorAll('.good.essay-test').length
+let reportCount= {
+	essay: getCount("essay-report"),
+	evaluation: getCount("evaluation-report")
 }
 
 let timesStyle = document.createElement('style');
@@ -24,37 +26,66 @@ document.getElementsByTagName('head').item(0).appendChild(timesStyle);
 let sheet = timesStyle.sheet,
 	idx   = sheet.length
 sheet.insertRule(`
-table.times td {
-	border-bottom: 1px solid #e9e9e9;
+table.counts td {
+	border-bottom: 1px solid #BDBDBD;
 	padding: 5px;
 }
 `,idx)
+sheet.insertRule(`
+table.counts th {
+	border-bottom: 1px solid #424242;
+	padding: 5px;
+	background-color: #EEEEEE;
+}
+`,idx+1)
+let counts = {
+	test: testCount.evaluation.all+testCount.essay.all,
+	report: reportCount.evaluation.all+reportCount.essay.all,
+	goods: {
+		test: testCount.evaluation.good+testCount.essay.good,
+		report: reportCount.evaluation.good+reportCount.essay.good,
+	}
+}
+counts.all  = counts.test+counts.report+movieCount.all
+counts.good = counts.goods.test+counts.goods.report+movieCount.good
 
 hyouji[0].innerHTML+=`
 <div class='u-card'>
 	<div class='u-list-header typo-list-title'>この単元の進捗状況</div>
 	<div class='u-card-inner'>
-		<table class='times'>
+		<table class='counts'>
 			<tr>
-				<td>すべての授業</td><td>${format(time)}</td>
+				<th>全体</th><th>${counts.good}/${counts.all}（${Math.round(counts.good/counts.all*100)}%）</th>
 			</tr>
 			<tr>
-				<td>視聴済み</td>    <td>${format(goodtime)}（${Math.round((goodtime / time) * 100)}%）</td>
+				<th>すべての授業</th><th>${format(time)}</th>
 			</tr>
 			<tr>
-				<td>未視聴</td>      <td>${format(time-goodtime)}</td>
+				<td>視聴済み</td><td>${format(goodtime)}（${Math.round((goodtime / time) * 100)}%）</td>
 			</tr>
 			<tr>
-				<td>授業動画数</td>   <td>${movieCount}</td>
+				<td>未視聴</td><td>${format(time-goodtime)}</td>
 			</tr>
 			<tr>
-				<td>すべてのテスト</td><td>${evaluationTestCount.good+essayTestCount.good}/${evaluationTestCount.all+essayTestCount.all}（${Math.round(((evaluationTestCount.good+essayTestCount.good)/(evaluationTestCount.all+essayTestCount.all))*100)}%）</td>
+				<td>授業動画数</td><td>${movieCount.good}/${movieCount.all}（${Math.round((movieCount.good / movieCount.all) * 100)}%）</td>
 			</tr>
 			<tr>
-				<td>記述/選択テスト</td><td>${evaluationTestCount.good}/${evaluationTestCount.all}（${Math.round(((evaluationTestCount.good)/(evaluationTestCount.all))*100)}%）</td>
+				<th>すべてのテスト</th><th>${testCount.evaluation.good+testCount.essay.good}/${testCount.evaluation.all+testCount.essay.all}（${Math.round(((testCount.evaluation.good+testCount.essay.good)/(testCount.evaluation.all+testCount.essay.all))*100)}%）</th>
 			</tr>
 			<tr>
-				<td>論述テスト</td><td>${essayTestCount.good}/${essayTestCount.all}（${Math.round(((essayTestCount.good)/(essayTestCount.all))*100)}%）</td>
+				<td>記述/選択テスト</td><td>${testCount.evaluation.good}/${testCount.evaluation.all}（${Math.round(((testCount.evaluation.good)/(testCount.evaluation.all))*100)}%）</td>
+			</tr>
+			<tr>
+				<td>論述テスト</td><td>${testCount.essay.good}/${testCount.essay.all}（${Math.round(((testCount.essay.good)/(testCount.essay.all))*100)}%）</td>
+			</tr>
+			<tr>
+				<th>すべてのレポート</th><th>${reportCount.evaluation.good+reportCount.essay.good}/${reportCount.evaluation.all+reportCount.essay.all}（${Math.round(((reportCount.evaluation.good+reportCount.essay.good)/(reportCount.evaluation.all+reportCount.essay.all))*100)}%）</th>
+			</tr>
+			<tr>
+				<td>記述/選択レポート</td><td>${reportCount.evaluation.good}/${reportCount.evaluation.all}（${Math.round(((reportCount.evaluation.good)/(reportCount.evaluation.all))*100)}%）</td>
+			</tr>
+			<tr>
+				<td>論述レポート</td><td>${reportCount.essay.good}/${reportCount.essay.all}（${Math.round(((reportCount.essay.good)/(reportCount.essay.all))*100)}%）</td>
 			</tr>
 		</table>
 	</div>
